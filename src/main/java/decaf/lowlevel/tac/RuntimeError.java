081b0e38be4bda2adf033b0da4a3266c743ca8be
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-12-13 11:29:41 +0800

    在类中添加了除零错误

--- src/main/java/decaf/lowlevel/tac/RuntimeError.java
+++ src/main/java/decaf/lowlevel/tac/RuntimeError.java
@@ -17,4 +17,6 @@ public final class RuntimeError {
     public static final String CLASS_CAST_ERROR2 = " cannot be cast to ";
 
     public static final String CLASS_CAST_ERROR3 = "\n";
+
+    public static final String DIV_BY_ZERO_ERROR = "Decaf runtime error: Division by zero error\n";
 }
